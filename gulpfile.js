de444bdc59e1f0b8cbcc17994fae023ece0a9c8d
codemansam <sam@stacked.co.nz>
2017-04-11 20:48:28 +1200

    Minified css file.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const 	gulp = require('gulp'),
 		imagemin = require('gulp-imagemin'),
 		uncss = require('gulp-uncss'),
+		cleanCSS = require('gulp-clean-css'),
 		concatCss = require('gulp-concat-css');
 
 gulp.task('images', () =>
@@ -17,12 +18,15 @@ gulp.task('uncss', function () {
         .pipe(gulp.dest('dist/css'));
 });
 
-
- 
 gulp.task('concatCss', function () {
   return gulp.src('dist/css/*.css')
     .pipe(concatCss("bundle.css"))
     .pipe(gulp.dest('dist/css'));
 });
-
+ 
+gulp.task('minify-css', function() {
+  return gulp.src('dist/css/bundle.css')
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('dist/css/final'));
+}); 
 gulp.task('default',['images','uncss', 'concatCss' ]);
