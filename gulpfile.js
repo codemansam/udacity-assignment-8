d3ffaa13e322336cba7d9ca6935f35d381be62b2
codemansam <sam@stacked.co.nz>
2017-04-11 15:51:23 +1200

    Added uncss to strip out unused css styles.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,19 @@
 const 	gulp = require('gulp'),
-		imagemin = require('gulp-imagemin');
+		imagemin = require('gulp-imagemin'),
+		uncss = require('gulp-uncss');
 
 gulp.task('images', () =>
     gulp.src('app/images/*')
         .pipe(imagemin())
         .pipe(gulp.dest('dist/images'))
 );
-gulp.task('default',['images',]);
+ 
+gulp.task('uncss', function () {
+    return gulp.src('app/css/*')
+        .pipe(uncss({
+            html: ['app/pizza.html']
+        }))
+        .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('default',['images','uncss']);
